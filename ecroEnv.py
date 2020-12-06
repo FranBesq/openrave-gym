17ff57c3bf2057b73526671d38289219d711fe8e
FranBesq <franciscobesq@gmail.com>
2020-12-06 18:38:41 +0100

    Added Q-Rl but gotta fix some obs_space type  problems

--- ecroEnv.py
+++ ecroEnv.py
@@ -1,21 +1,17 @@
+#!/usr/bin/env python
+
 import gym
+import time
 import numpy as np
 from gym import spaces
 from openravepy import *
-
-#Dimensions of input image
-HEIGHT = 416
-WIDTH = 416
-#Patch size
-ACTION_H = 52
-ACTION_W = 96
 
 class EcroEnv(gym.Env):
   """Custom Environment to control Ecron robot in openrave using gym interface"""
   metadata = {'render.modes': ['human']}
 
   def __init__(self, orenv=None, normActions=True):
-
+    
     self.orenv = orenv #Openrave env
     self.normActionSpace = True
     if normActions == False:
@@ -23,7 +19,7 @@ class EcroEnv(gym.Env):
     else:
       self.normActions = True
 
-    super(AttackEnv, self).__init__()
+    super(EcroEnv, self).__init__()
     # Define action and observation space
     # Action space is velocity of each weel 
     if self.normActionSpace is True:
@@ -34,34 +30,35 @@ class EcroEnv(gym.Env):
                     (4,), dtype=np.uint8)
     # ObsSpace shape pending:
     self.observation_space = spaces.Box(low=0, high=255, shape=
-                    (HEIGHT, WIDTH, 3), dtype=np.uint8)
+                    (4, 4), dtype=np.float32) # H matrix shape 
 
   def step(self, action):
-        #Quizas hay que formatear la accion de otra forma
-        velocities = [i*10 for i in action]
-        self.control.SetDesired(action)
-        time.sleep(0.5)#Let robot in that direction for sleep time
+    #Quizas hay que formatear la accion de otra forma
+    velocities = [i*10 for i in action]
+    self.control.SetDesired(action)
+    time.sleep(0.5)#Let robot in that direction for sleep time
 
-        H_0_robot = self.robot.GetTransform()
-        print("Shape of H: " + str(H_0_robot.shape()))
-        print(H_0_robot)
-    ...
+    H_0_robot = self.robot.GetTransform()
+    print(H_0_robot)
+
+    return H_0_robot#, 10, False, None
+    
   def reset(self):
     # Reset the state of the environment to initial position
     self.orenv.Load('/home/francisco/Documents/Robot_Sim/openrave-gym/map.env.xml') # load a simple scene
     self.robot = self.orenv.GetRobots()[0] # get the first robot
-    print("Using robot: ", robot.GetName())
-    self.robot.SetController(RaveCreateController(self.orenv,'idealvelocitycontroller'),range(robot.GetDOF()),0)
-    self.control = control = robot.GetController()
+    print("Using robot: ", self.robot.GetName())
+    self.robot.SetController(RaveCreateController(self.orenv,'idealvelocitycontroller'),range(self.robot.GetDOF()),0)
+    self.control = self.robot.GetController()
 
     self.orenv.StartSimulation(timestep=0.001)
 
     return self._get_obs() # a falta de definir una observacion
     
-    ...
+    
   def render(self, mode='human', close=False):
     # Render the environment to the screen
-    ...
+    return 0
 
   def _get_obs(self):
     return self.robot.GetTransform()
--- testOREcron.py
+++ testOREcron.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from openravepy import *
+import scipy
 import time
 
 env = Environment() # create openrave environment
@@ -13,17 +14,16 @@ try:
     print("Using robot: ", robot.GetName())
     robot.SetController(RaveCreateController(env,'idealvelocitycontroller'),range(robot.GetDOF()),0)
     control = robot.GetController()
-
+    time.sleep(3)
     env.StartSimulation(timestep=0.001)
 
-    for i in range(20):
+    for i in range(5):
         velocities = [10.0, 10.0, 10.0, 10.0]
         control.SetDesired(velocities)
 
         time.sleep(0.5)
 
         H_0_robot = robot.GetTransform()
-        print("Shape of H: " + str(H_0_robot.shape()))
         print(H_0_robot)
 
     time.sleep(1)
