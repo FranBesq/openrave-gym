dc7373e818eec9ed7b1e3bc0ea3fe91d91130b48
FranBesq <franciscobesq@gmail.com>
2020-12-07 12:50:11 +0100

    Fixed various bugs

--- ecroEnv.py
+++ ecroEnv.py
@@ -1,17 +1,22 @@
-#!/usr/bin/env python
-
 import gym
 import time
 import numpy as np
 from gym import spaces
 from openravepy import *
 
+#Dimensions of input image
+HEIGHT = 416
+WIDTH = 416
+#Patch size
+ACTION_H = 52
+ACTION_W = 96
+
 class EcroEnv(gym.Env):
   """Custom Environment to control Ecron robot in openrave using gym interface"""
   metadata = {'render.modes': ['human']}
 
   def __init__(self, orenv=None, normActions=True):
-    
+
     self.orenv = orenv #Openrave env
     self.normActionSpace = True
     if normActions == False:
@@ -24,25 +29,25 @@ class EcroEnv(gym.Env):
     # Action space is velocity of each weel 
     if self.normActionSpace is True:
         self.action_space = spaces.Box(low=0, high=1, shape=
-                    (4,), dtype=np.float32)
+                    (4,), dtype=float)
     else:
       self.action_space = spaces.Box(low=0, high=10, shape=
                     (4,), dtype=np.uint8)
     # ObsSpace shape pending:
     self.observation_space = spaces.Box(low=0, high=255, shape=
-                    (4, 4), dtype=np.float32) # H matrix shape 
+                    (HEIGHT, WIDTH, 3), dtype=np.uint8)
 
   def step(self, action):
-    #Quizas hay que formatear la accion de otra forma
-    velocities = [i*10 for i in action]
-    self.control.SetDesired(action)
-    time.sleep(0.5)#Let robot in that direction for sleep time
+        #Quizas hay que formatear la accion de otra forma
+        velocities = [i*10 for i in action]
+        self.control.SetDesired(action)
+        time.sleep(0.5)#Let robot in that direction for sleep time
 
-    H_0_robot = self.robot.GetTransform()
-    print(H_0_robot)
+        H_0_robot = self.robot.GetTransform()
+        print(H_0_robot)
 
-    return H_0_robot#, 10, False, None
-    
+        return H_0_robot
+
   def reset(self):
     # Reset the state of the environment to initial position
     self.orenv.Load('/home/francisco/Documents/Robot_Sim/openrave-gym/map.env.xml') # load a simple scene
@@ -54,11 +59,11 @@ class EcroEnv(gym.Env):
     self.orenv.StartSimulation(timestep=0.001)
 
     return self._get_obs() # a falta de definir una observacion
-    
+
     
   def render(self, mode='human', close=False):
     # Render the environment to the screen
     return 0
 
   def _get_obs(self):
-    return self.robot.GetTransform()
+    return self.robot.GetTransform() 
